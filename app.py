bc147cf4a04b7a84c34b21ffd83155a8e2d32444
florian <florian.badoules@gmail.com>
2025-01-30 13:40:14 +0100

    Ajout de la route / pour éviter l'erreur 404

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    data = request.json
-    streams = float(data['streams']) * 0.003  # 0.003 € par stream
-    physical_sales = float(data['physicalSales']) * 10  # 10 € par vente physique
-    digital_sales = float(data['digitalSales']) * 5  # 5 € par vente digitale
-    revenue = streams + physical_sales + digital_sales
-    return jsonify({'revenue': round(revenue, 2)})
+@app.route('/')
+def home():
+    return "L'API est en ligne !"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
